973c4bb4f02df88f5770514f6eb88f85a682caf6
fabricio chaile <fabri_chaile@hotmail.com>
2024-05-20 19:53:12 -0300

    ejercicio 2 hecho

--- ejercicio1.ts
+++ ejercicio1.ts
@@ -1,1 +1,21 @@
+// realice un programa que devuelva la potencia de un numero.
+// la base y el exponente deben ser ingresados por teclado.
+// solo deben admitirse exponentes mayores o iguales a cero.
+// recuerde que el resultado de un numero elevado a 0 es 1.
+// separe la logica de calcular la potencia utilizando metodos.
+
 import * as rls from 'readline-sync';
+
+let base: number = rls.questionInt ("Ingrese la base del calculo");
+let exponente: number = rls.questionInt ("Ingrese el exponente del calculo");
+
+while (exponente < 0){
+    console.log ("ingresar exponente mayor o igual a cero");
+    exponente = rls.questionInt("ingrese numero de exponente");
+}
+
+function calculo (base: number, exponente:number){
+    let resultado:number;
+    resultado= base**exponente;
+    return resultado;
+}
